d0eb61827aa9c1eeb7665765a317ce5451477b72
marcin6 <marcindelert@gmail.com>
2021-01-10 00:21:28 +0100

    update project

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,62 +1,50 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import UsersList from './UsersList';
 import '../styles/Users.css'
 
-export default class Users extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            userList: [],
-        }
-    }
+export default function Users() {
+    const [userList, setUserList] = useState([]);
+    const [value, setValue] = useState("");
+    const [errorMsg, setErrorMsg] = useState("")
 
-    addUser = (e) => {
+    const addUser = e => {
         e.preventDefault();
-        if (this.validate(this.fileInput)) {
+        if (validate(value)) {
             let newUser = {
                 key: Date.now(),
-                userName: this.fileInput.value,
+                userName: value,
             }
-            this.setState((prevState) => {
-                return ({
-                    userList: prevState.userList.concat(newUser),
-                    errorMsg: "",
-                })
-            })
-            this.fileInput.value = "";
+            setUserList([...userList, newUser]);
+            setValue("");
+            setErrorMsg("");
         } else {
-            this.setState({
-                errorMsg: "Enter name!!",
-            });
-        }   
-    }
-
-    removeUser = (userKey) => {
-        let filteredUser = this.state.userList.filter((user) => user.key !== userKey);
-        this.setState({
-            userList: filteredUser,
-        });
-    }
-
-    validate = (item) => {
-        if (item.value === "") {
-            return false;
-        } else {
-            return true;
+            setErrorMsg("Enter name!!");
         }
     }
 
-    render() {
-        return (
-            <div className="users-main">
-                <h1>User's List</h1>
-                <form onSubmit={this.addUser}>
-                    <input ref={(input) => this.fileInput = input} type="text" placeholder="Enter name"></input>
-                    <button type="submit">Add users</button>
-                </form>
-                <div className="error-msg">{this.state.errorMsg}</div>
-                <UsersList removeUser={this.removeUser} usersList={this.state.userList} />
-            </div>
-        )
+    const removeUser = (userKey) => {
+        let filteredUser = userList.filter((user) => user.key !== userKey);
+        setUserList(filteredUser);
+     }
+
+    const validate = (item) => {
+        if (item === "") {
+            console.log("false")
+            return false
+        } else {
+            console.log("true")
+            return true
+        }
     }
+
+    return (
+        <div className="users-main">
+            <form onSubmit={addUser}>
+                <input value={value} onChange={e => setValue(e.target.value)} type="text" placeholder="Enter name"></input>
+                <button type="submit">Add users</button>
+            </form>
+            <div className="error-msg">{errorMsg}</div>
+            <UsersList removeUser={removeUser} usersList={userList} />
+        </div>
+    )
 }
